a56e21f4aabef9660e0c16c6d73fa9eda2be0600
Nikita Isyuk <Nikita.Isyuk@outlook.com>
2022-04-26 13:37:57 +0300

    WPF

---
+++ CourseProjectViewWPF/Services/DialogWindowService.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace CourseProjectViewWPF.Services
+{
+    public class DialogWindowService : IWindowService
+    {
+        public void showWindow(object viewModel)
+        {
+            var win = new Window();
+            win.Content = viewModel;
+            win.ShowDialog();
+        }
+    }
+}
