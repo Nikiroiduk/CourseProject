0a14a1e78fb040529b135807fb75df5628b341c4
Nikita Isyuk <Nikita.Isyuk@outlook.com>
2022-05-09 13:39:12 +0300

    meh

--- CourseProjectViewWPF/View/test.xaml.cs
+++ CourseProjectViewWPF/View/test.xaml.cs
@@ -1,27 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 
 namespace CourseProjectViewWPF.View
 {
     /// <summary>
-    /// Interaction logic for test.xaml
+    /// Interaction logic for MaterialCards.xaml
     /// </summary>
-    public partial class test : Window
+    public partial class test : Window //INotifyPropertyChanged
     {
+        private double _lastLecture;
+
         public test()
         {
             InitializeComponent();
+
+            LastHourSeries = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    AreaLimit = -10,
+                    Values = new ChartValues<ObservableValue>
+                    {
+                        new ObservableValue(3),
+                        new ObservableValue(5),
+                        new ObservableValue(6),
+                        new ObservableValue(7),
+                        new ObservableValue(3),
+                        new ObservableValue(4),
+                        new ObservableValue(2),
+                        new ObservableValue(5),
+                        new ObservableValue(8),
+                        new ObservableValue(3),
+                        new ObservableValue(5),
+                        new ObservableValue(6),
+                        new ObservableValue(7),
+                        new ObservableValue(3),
+                        new ObservableValue(4),
+                        new ObservableValue(2),
+                        new ObservableValue(5),
+                        new ObservableValue(8)
+                    }
+                }
+            };
+
+            DataContext = this;
+        }
+
+        public SeriesCollection LastHourSeries { get; set; }
+
+        public double LastLecture
+        {
+            get { return _lastLecture; }
+            set
+            {
+                _lastLecture = value;
+                //OnPropertyChanged("LastLecture");
+            }
+        }
+
+        ////public event PropertyChangedEventHandler PropertyChanged;
+
+        ////protected virtual void OnPropertyChanged(string propertyName = null)
+        ////{
+        ////    var handler = PropertyChanged;
+        ////    if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        ////}
+
+        private void UpdateOnclick(object sender, RoutedEventArgs e)
+        {
+            TimePowerChart.Update(true);
         }
     }
 }
